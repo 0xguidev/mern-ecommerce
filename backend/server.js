a3ec0c1c9411dcb708282f9d9ee6b26ac2c7589f
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-07-26 13:53:57 -0300

    initial project

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const products = require('./data/products');
-const cors = require('cors');
-required('dotenv').config();
+import express from 'express';
+import products from './data/products.js';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -9,16 +9,16 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+  res.send('API is running...');
 });
 
 app.get('/api/products', (_req, res) => {
-    res.status(200).json(products);
-})
+  res.status(200).json(products);
+});
 
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find((prod) => prod._id === req.params.id)
-    res.json(product);
-})
+  const product = products.find((prod) => prod._id === req.params.id);
+  res.json(product);
+});
 
 app.listen(port, console.log(`Server runner on port ${port}`));
