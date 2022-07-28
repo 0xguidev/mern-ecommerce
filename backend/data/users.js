985a7f0042b8e888926882e957e7dfac0110e093
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-07-28 13:58:06 -0300

    criando seed de usuarios

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('umaSenhaIncrivelmenteDificil', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'john doe',
+    email: 'johndoe@example.com',
+    password: bcrypt.hashSync('umaSenhaIncrivelmenteDificil', 10),
+  },
+  {
+    name: 'gui de paula',
+    email: 'guidepaula@example.com',
+    password: bcrypt.hashSync('umaSenhaIncrivelmenteDificil', 10),
+  },
+];
+
+export default users;
