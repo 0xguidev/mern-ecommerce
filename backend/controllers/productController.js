2532dc4a79769061e7066384e15513f22dd33e43
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-09-16 07:41:57 -0300

    create usercontroller and refactor userRoutes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-import Product from "../models/productModel";
+import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
 
 // @desc    Fetch all products
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import 'dotenv/config';
 import 'colors';
 import connectDB from './config/db.js';
 import productRouter from './routes/productRoutes.js';
-import { errorHandle, notFound } from './middleware/errorMiddleware.js';
+import userRoutes from "./routes/userRoutes.js";
+import {errorHandle, notFound} from './middleware/errorMiddleware.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -12,12 +13,14 @@ const port = process.env.PORT;
 connectDB();
 
 app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('API is running...');
+    res.send('API is running...');
 });
 
 app.use('/api/products', productRouter);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
