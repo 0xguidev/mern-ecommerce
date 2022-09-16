bbdfa3ea9d7c683ae5f9cc2599bdb3ed78b7d0c6
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-09-16 09:37:41 -0300

    feature: new endpoint get user profile, new middleware authentication

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async(req, res, next) => {
+    let token
+    const auth = req.headers.authorization
+    if(auth && auth.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            req.user = await User.findById(decoded.id).select('-password')
+            next()
+        } catch (e) {
+            console.error(e.message)
+            res.status(401).json({message: "not authorized, token fail"})
+        }
+    }
+})
+export default protect
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { authUser } from "../controllers/useController.js";
+import {authUser, getUserProfile} from "../controllers/useController.js";
+import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/login', authUser)
+router.get('/profile', protect, getUserProfile)
 
 export default router
