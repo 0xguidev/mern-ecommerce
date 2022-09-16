fea2391c0a71015c973003efec2beb21d66ff25e
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-09-16 10:23:09 -0300

    feature: new endpoint register user

--- backend/controllers/useController.js
+++ backend/controllers/useController.js
@@ -38,7 +38,6 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({
         name, email, password
     })
-    console.log(user)
 
     if(user) {
         res.status(200).json({message: {
