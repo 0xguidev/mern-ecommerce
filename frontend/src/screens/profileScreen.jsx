6e8b19bb942c2128c1cd16581cd0440e88178c53
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-09-18 17:21:46 -0300

    Refactor: profileScreen.jsx

--- frontend/src/screens/profileScreen.jsx
+++ frontend/src/screens/profileScreen.jsx
@@ -58,67 +58,66 @@ function ProfileScreen() {
     }
 
     return (
-        <Container>
-            <Form onSubmit={handleSubmit}>
-                <Row className='justify-content-center'>
-                    <Col xs={6} md={3}>
-                        <h2>Edit Profile</h2>
-                        <Form.Group className="mb-3" controlId="formBasicName">
-                            <Form.Label>Full Name</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter name"
-                                value={user.name}
-                                disabled={true}
-                            />
-                        </Form.Group>
+        <Row>
+            <Col md={3}>
+                <Form onSubmit={handleSubmit}>
+                    <h2>Edit Profile</h2>
+                    <Form.Group className="mb-3" controlId="updateName">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter name"
+                            value={user.name}
+                            disabled={true}
+                        />
+                    </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="Enter email"
-                                value={user.email}
-                                disabled={true}
-                            />
-                        </Form.Group>
+                    <Form.Group className="mb-3" controlId="updateEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            value={user.email}
+                            disabled={true}
+                        />
+                    </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>New password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Password"
-                                onChange={(e) => setUserData({...userData, password: e.target.value})}
-                            />
-                        </Form.Group>
+                    <Form.Group className="mb-3" controlId="updatePassword">
+                        <Form.Label>New password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            onChange={(e) => setUserData({...userData, password: e.target.value})}
+                        />
+                    </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Confirm password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="Confirm password"
-                                onChange={(e) => setUserData({...userData, confirmPassord: e.target.value})}
-                            />
-                        </Form.Group>
+                    <Form.Group className="mb-3" controlId="updateConfirmPassword">
+                        <Form.Label>Confirm password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Confirm password"
+                            onChange={(e) => setUserData({...userData, confirmPassord: e.target.value})}
+                        />
+                    </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out"/>
-                        </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
 
-                        <Button
-                            variant="primary"
-                            type="submit"
-                            disabled={userData.isDisabled}
-                        >
-                            Update
-                        </Button>
-                        {
-                            (isError) ?? <Message varaiant={'danger'}>{error}</Message>
-                        }
-                    </Col>
-                </Row>
-            </Form>
-        </Container>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        disabled={userData.isDisabled}
+                    >
+                        Update
+                    </Button>
+                    {
+                        (isError) ?? <Message varaiant={'danger'}>{error}</Message>
+                    }
+                </Form>
+            </Col>
+            <Col md={9}><h1>My Orders</h1></Col>
+        </Row>
     );
 }
 
