2a2664182100974e2d8f524fec727656246b1533
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-09-18 11:29:24 -0300

    feature, a new card

--- frontend/src/screens/profileScreen.jsx
+++ frontend/src/screens/profileScreen.jsx
@@ -8,6 +8,10 @@ function ProfileScreen() {
                 <Card.Link href="#">Card Link</Card.Link>
                 <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
+            <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
         </Card>
     );
 }
