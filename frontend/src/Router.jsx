8c9e367c771c4a0c382afde97063ddda3e116347
0xguidev <guilhermeviniciustrabalho@gmail.com>
2022-09-19 15:14:58 -0300

    Feature: create Shipping Screen

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 import SingUpScreen from './screens/SingUpScreen';
-import LoginScreen from "./screens/LoginScreen";
-import LogoutScreen from "./screens/LogoutScreen";
-import ProfileScreen from "./screens/profileScreen";
+import LoginScreen from './screens/LoginScreen';
+import LogoutScreen from './screens/LogoutScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route exact path="/" element={<HomeScreen/>}/>
-            <Route exact path="/login" element={<LoginScreen/>}/>
-            <Route exact path="/product/:id" element={<ProductScreen/>}/>
-            <Route path="/cart">
-                <Route path=":id" element={<CartScreen/>}/>
-                <Route path="" element={<CartScreen/>}/>
-            </Route>
-            <Route exact path="/logout" element={<LogoutScreen/>}/>
-            <Route exact path="/singup" element={<SingUpScreen/>}/>
-            <Route exact path="/profile" element={<ProfileScreen/>} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route exact path="/" element={<HomeScreen />} />
+      <Route exact path="/login" element={<LoginScreen />} />
+      <Route exact path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart">
+        <Route path=":id" element={<CartScreen />} />
+        <Route path="" element={<CartScreen />} />
+      </Route>
+      <Route exact path="/logout" element={<LogoutScreen />} />
+      <Route exact path="/singup" element={<SingUpScreen />} />
+      <Route exact path="/profile" element={<ProfileScreen />} />
+      <Route exact path="/shipping" element={<ShippingScreen />} />
+    </Routes>
+  );
 };
 
 export default Router;
